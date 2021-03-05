6f7ccc8adf87670cedc4a2e9af65cc0bcde457f1
oluSammy <olumorinsammy@gmail.com>
2021-03-05 14:55:05 -0800

    save activeclass

--- src/Components/Sidebar/Sidebar.component.jsx
+++ src/Components/Sidebar/Sidebar.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import sidebarStyles from './Sidebar.styles';
 import vaultWayLogo from '../../assets/icons/vault-way-white-icon.png';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation  } from 'react-router-dom';
 import HomeIcon from '@material-ui/icons/Home';
 import DashboardIcon from '@material-ui/icons/Dashboard';
 import InfoIcon from '@material-ui/icons/Info';
@@ -10,24 +10,27 @@ import HelpIcon from '@material-ui/icons/Help';
 import VpnKeyIcon from '@material-ui/icons/VpnKey';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import Collapse from '@material-ui/core/Collapse';
-import { NavHashLink } from 'react-router-hash-link';
+import { HashLink } from 'react-router-hash-link';
 import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import GraphicEqOutlinedIcon from '@material-ui/icons/GraphicEqOutlined';
 import GrainOutlinedIcon from '@material-ui/icons/GrainOutlined';
 import BlurOnOutlinedIcon from '@material-ui/icons/BlurOnOutlined';
+import clsx from 'clsx';
 
 
 const Sidebar = () => {
   const classes = sidebarStyles();
   const [open, setOpen] = React.useState(false);
+  let location = useLocation ();
+  const savePage = location.pathname === '/plans';
 
   const handleClick = () => {
     setOpen(!open);
   };
   const navLinks = [
     {
-      link: "/",
+      link: "/dashboard",
       icon: <DashboardIcon className={classes.navIcon} />,
       text: 'Dashboard',
       dropDown: false
@@ -76,25 +79,25 @@ const Sidebar = () => {
         </NavLink>
         {navLinks.map(link =>
           link.dropDown ?
-          <div>
-            <div className={classes.navItem} activeClassName={classes.activeNavLink} onClick={handleClick}>
+          <div key={link.text}>
+            <div className={clsx(classes.navItem, savePage ? classes.activeNavLink : '' )} onClick={handleClick}>
               {link.icon}
               <span className={classes.navText}>Save</span>
               {link.endIcon}
             </div>
             <Collapse in={open} timeout="auto" unmountOnExit style={{marginLeft: '30px'}}>
-                <NavHashLink to="/plans#regular" className={classes.navItem} >
+                <HashLink to="/plans#regular" className={classes.navItem} >
                   <GraphicEqOutlinedIcon className={classes.navIcon} />
                     <span className={classes.navText}>Regular Vault</span>
-                </NavHashLink>
-                <NavHashLink to="/plans#quick" className={classes.navItem} >
+                </HashLink>
+                <HashLink to="/plans#quick" className={classes.navItem} >
                   <GrainOutlinedIcon className={classes.navIcon} />
                     <span className={classes.navText}>Quick Vault</span>
-                </NavHashLink>
-                <NavHashLink to="/plans#fundMyVault" className={classes.navItem} >
+                </HashLink>
+                <HashLink to="#fundMyVault" className={classes.navItem} >
                   <BlurOnOutlinedIcon className={classes.navIcon} />
                     <span className={classes.navText}>FundMyVault</span>
-                </NavHashLink>
+                </HashLink>
             </Collapse>
           </div>
              :
